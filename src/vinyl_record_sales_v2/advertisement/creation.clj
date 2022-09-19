a7e60b6efb2d04e55ac0ea10333b73a9f43d195b
Andreas Nilsson <anil9@kth.se>
2022-09-19 18:34:01 +0200

    Extracted upload

--- src/vinyl_record_sales_v2/advertisement/creation.clj
+++ src/vinyl_record_sales_v2/advertisement/creation.clj
@@ -1,62 +1,26 @@
 (ns vinyl-record-sales-v2.advertisement.creation
-  (:require [clojure.string :as s]
-            [clj-http.client :as client]
-            [vinyl-record-sales-v2.advertisement.text-resolver :as text]
+  (:require [vinyl-record-sales-v2.advertisement.text-resolver :as text]
             [vinyl-record-sales-v2.advertisement.category-resolver :as category]))
 
-
-(def sello-token (s/trim-newline (slurp "/home/andreas/sello/api.token")))
-(def base-url "https://api.sello.io/v5/")
 (def folder-id 1102266)
 (def tradera-id 53501)
 
-(def client-map
-  {:as :auto
-   :headers {:authorization sello-token}
-   :content-type :json}) 
-
-(comment
-  (client/get (str base-url "products") (assoc-in client-map [:headers :authorization] sello-token))
-  (client/get (str base-url "products/64536005") (assoc-in client-map [:headers :authorization] sello-token)))
-
-(defn upload-ad! [ad]
-  (client/post (str base-url "products")
-               (assoc client-map :form-params ad)))
-(declare example-ad)
-(comment
-  (upload-ad! example-ad)
-  (assoc client-map :form-params example-ad))
 
 (def template-ad
   {:purchase_price 0.5,
    :tax 25,
    :folder_id folder-id,
    :prices {(keyword (str tradera-id)) {:auction {:start 25}, :currency "SEK"}}
-   :categories {:default {:id 340634}}
    :condition "used",
    :shipping {(keyword (str tradera-id)) {:pickup true :schenker "70.00"}},
    :quantity 1})
 
 
 (defn create-ad [record-info]
-  ;(text/create-text record-info))
   (-> template-ad
       (assoc-in [:texts :default :sv :description] (text/create-text record-info)) 
       (assoc-in [:texts :default :sv :name] (:title record-info)) 
       (assoc-in [:categories :default :id] (category/category-id record-info))))
-      
-  
-
-(def example-ad {:texts {:default {:sv {:description "Skick se bilder. Ej provspelad. Kolla gärna in mina andra annonser och passa på att buda in fler LP-skivor, jag samfraktar gärna!<br>Fraktpriser (ca):<br>1 skiva 70 kr<br>2 skivor 73 kr<br>3 skivor 76 kr<br>4 skivor 87 kr<br>5 skivor 99 kr<br>6 skivor 102 kr<br><br>År: 1983<br>Genre: Pop, Folk, World, & Country<br>Style: Vocal, Folk, Schlager<br>Tracklist:<br>Sinatra In Concert 4:50<br>Sommar Som Vilar I Gräset 2:39<br>Om Du Nånsin Kommer Fram Till Samarkand 4:22<br>I Write The Songs 2:42<br>Den Första Gång Jag Såg Dig 3:17<br>Send In The Clowns 4:28<br>Ge Mig En Dag 1:59<br>Den Glade Bagarn 3:00<br>Annies Song 2:37<br>Minnet = Memory 3:50<br>I Left My Heart In San Francisco 3:38<br>Romantik 3:56<br>Jubileumsblues 3:10<br>Kullerullvisan 3:16<br>Ha Det Bra ",
-                                        :name "Ludvika Musikkår - Sonja Stjernquist - Ha Det Bra"}}}
-                 :purchase_price 0.5,
-                 :tax 25,
-                 :folder_id folder-id,
-                 :prices {(keyword (str tradera-id)) {:auction {:start 25}, :currency "SEK"}}
-                 :categories {:default {:id 340634}}
-                 :condition "used",
-                 :shipping {(keyword (str tradera-id)) {:pickup true :schenker "70.00"}},
-                 :quantity 1})
 
 ; :body {:properties [],
 ;        :submitter_id nil,
---
+++ src/vinyl_record_sales_v2/advertisement/upload.clj
@@ -0,0 +1,37 @@
+(ns vinyl-record-sales-v2.advertisement.upload
+  (:require [clj-http.client :as client]
+            [clojure.string :as s]))
+
+(def sello-token (s/trim-newline (slurp "/home/andreas/sello/api.token")))
+(def base-url "https://api.sello.io/v5/")
+(def client-map
+  {:as :auto
+   :headers {:authorization sello-token}
+   :content-type :json})
+(declare example-ad)
+
+(defn upload-ad! [ad]
+  (client/post (str base-url "products")
+               (assoc client-map :form-params ad)))
+
+
+(comment
+  (client/get (str base-url "products") (assoc-in client-map [:headers :authorization] sello-token))
+  (client/get (str base-url "products/64536005") (assoc-in client-map [:headers :authorization] sello-token)))
+
+
+(comment
+  (def folder-id 1102266)
+  (def tradera-id 53501)
+  (def example-ad {:texts {:default {:sv {:description "Skick se bilder. Ej provspelad. Kolla gärna in mina andra annonser och passa på att buda in fler LP-skivor, jag samfraktar gärna!<br>Fraktpriser (ca):<br>1 skiva 70 kr<br>2 skivor 73 kr<br>3 skivor 76 kr<br>4 skivor 87 kr<br>5 skivor 99 kr<br>6 skivor 102 kr<br><br>År: 1983<br>Genre: Pop, Folk, World, & Country<br>Style: Vocal, Folk, Schlager<br>Tracklist:<br>Sinatra In Concert 4:50<br>Sommar Som Vilar I Gräset 2:39<br>Om Du Nånsin Kommer Fram Till Samarkand 4:22<br>I Write The Songs 2:42<br>Den Första Gång Jag Såg Dig 3:17<br>Send In The Clowns 4:28<br>Ge Mig En Dag 1:59<br>Den Glade Bagarn 3:00<br>Annies Song 2:37<br>Minnet = Memory 3:50<br>I Left My Heart In San Francisco 3:38<br>Romantik 3:56<br>Jubileumsblues 3:10<br>Kullerullvisan 3:16<br>Ha Det Bra ",
+                                          :name "Ludvika Musikkår - Sonja Stjernquist - Ha Det Bra"}}}
+                   :purchase_price 0.5,
+                   :tax 25,
+                   :folder_id folder-id,
+                   :prices {(keyword (str tradera-id)) {:auction {:start 25}, :currency "SEK"}}
+                   :categories {:default {:id 340634}}
+                   :condition "used",
+                   :shipping {(keyword (str tradera-id)) {:pickup true :schenker "70.00"}},
+                   :quantity 1})
+  (upload-ad! example-ad)
+  (assoc client-map :form-params example-ad))
